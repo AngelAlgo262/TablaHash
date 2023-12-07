6a95a5fc7871cd758199591994a7894ead76e158
angelocampo262 <128538961+angelocampo262@users.noreply.github.com>
2023-12-07 07:07:36 -0600

    FIX menu and Function Hash

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -10,10 +9,11 @@ public class Main {
     private static final TablaHash tabla = new TablaHash(); // Crear una instancia de TablaHash
 
     public static void main(String[] args) {
-        login();
-        menu();
+        login(); //llamar función login
+        menu(); //llamar función menu
     }
 
+    //función login
     public static void login() {
         boolean iniciar = false;
         String usuario = "Administrador";
@@ -21,10 +21,10 @@ public class Main {
         while (!iniciar) {
             System.out.println("Usuario:");
             //scanner.nextLine();
-            String userTrabajador = scanner.nextLine();
+            String userLogin = scanner.nextLine();
             System.out.println("Contraseña:");
-            String passTrabajador = scanner.nextLine();
-            if (!userTrabajador.equals(usuario) || !passTrabajador.equals(pass)) {
+            String passLogin = scanner.nextLine();
+            if (!userLogin.equals(usuario) || !passLogin.equals(pass)) {
                 iniciar = false;
                 System.out.println("Las credenciales no son correctas, intente de nuevo");
             } else {
@@ -33,7 +33,8 @@ public class Main {
             }
         }
     }
-    //clase para el menu
+
+    //función para el menu
     public static void menu() {
         int option;
         boolean salir = false;
@@ -41,39 +42,59 @@ public class Main {
             System.out.println("\nSelecciona una opción");
             System.out.println("1. Crear un usuarios");
             System.out.println("2. Ver tabla hash");
-            System.out.println("3. Modificar usuario");
-            System.out.println("4. Buscar usuario");
-            System.out.println("5. Eliminar usuario");
-            System.out.println("6. Salir");
+            System.out.println("3. Buscar usuario");
+            System.out.println("4. Eliminar usuario");
+            System.out.println("5. Salir");
             option = scanner.nextInt();
-            //bucle para cada uno de los casos
-            switch (option) {
-                case 1:
-                    crearUsuario();
-                    break;
-                case 2:
-                    tabla.verTabla();
-                    break;
-                case 3:
-                    //modificarUsuario();
-                    break;
-                case 4:
-                    System.out.println("Caso cuatro");
-                    break;
-                case 5:
-                    System.out.println("Caso cinco");
-                    break;
-                case 6:
-                    salir = true;
-                    System.out.println("Hasta luego!!!!");
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor, selecciona una opción valida.");
+            try {
+                // Leer la entrada como string
+                String input = scanner.nextLine();
+                scanner.nextLine(); // Consumir el salto de línea
+                option = Integer.parseInt(input);
+                switch (option) {
+                    case 1:
+                        crearUsuario();
+                        break;
+                    case 2:
+                        tabla.verTabla();
+                        break;
+                    case 3:
+                        System.out.println("Indique el nombre del usuario a buscar");
+                        scanner.nextLine();
+                        String userToFind = scanner.nextLine();
+                        Usuario usuarioEncontrado = tabla.obtenerUsuario(userToFind);
+                        if (usuarioEncontrado != null) {
+                            System.out.println("Usuario encontrado:");
+                            System.out.println("Nombre de Usuario: " + usuarioEncontrado.getNombre());
+                            System.out.println("Contraseña: " + usuarioEncontrado.getPass());
+                        } else {
+                            System.out.println("Usuario no encontrado");
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Indique el nombre del usuario a eliminar");
+                        scanner.nextLine();
+                        String userToDelete = scanner.nextLine();
+                        tabla.eliminarUsuario(userToDelete);
+                        System.out.println("Usuario elimina correctamente");
+                        break;
+                    case 5:
+                        salir = true;
+                        System.out.println("Hasta luego!!!!");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Por favor, selecciona una opción valida.");
+                        scanner.nextLine(); // Limpiar el buffer de entrada
+                        break;
+                }
+            }catch (NumberFormatException e) {
+                System.out.println("Entrada no válida. Por favor, ingrese un número.");
             }
         }
     }
-//Crear usuario
-    public static void crearUsuario(){
+
+    //Crear usuario
+    public static void crearUsuario() {
         System.out.println("Ingrese el nombre de usuario:");
         scanner.nextLine(); //consumir la entrada de datos en una nueva linea
         String nombreUsuario = scanner.nextLine();
@@ -83,8 +104,9 @@ public class Main {
 
         tabla.agregarUsuario(nombreUsuario, hashPass);
     }
-//función para cifrar la contraseña
-    public static String obtenerHash(String pass){
+
+    //función para cifrar la contraseña
+    public static String obtenerHash(String pass) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(pass.getBytes(StandardCharsets.UTF_8));
--- src/TablaHash.java
+++ src/TablaHash.java
@@ -1,18 +1,21 @@
 import java.util.LinkedList;
-
+//función para generar tabla
 public class TablaHash {
-    private static final int TAMANO = 5; //tamaño de la tabla
+    private static final int TAMANO = 2000; //tamaño de la tabla
     private LinkedList<Usuario>[] tabla; //Arreglo para manejar colisiones
-
     public TablaHash() {
         tabla = new LinkedList[TAMANO];
         for (int i = 0; i < TAMANO; i++) {
             tabla[i] = new LinkedList<>();
         }
     }
-
+    //Función hash
     private int funcionHash(String nombreUsuario) {
-        return nombreUsuario.length() % TAMANO;
+        int hash = 7;
+        for (int i = 0; i < nombreUsuario.length(); i++){
+            hash = hash * 31 + nombreUsuario.charAt(i);
+        }
+        return Math.abs(hash) % TAMANO;
     }
 
     //Agregar usuario
@@ -32,7 +35,7 @@ public class TablaHash {
         }
         return null; // Usuario no encontrado
     }
-    // Método para mostrar la tabla hash
+    // Función para mostrar la tabla hash
     public void verTabla() {
         System.out.println("Contenido de la tabla hash:");
         for (int i = 0; i < TAMANO; i++) {
